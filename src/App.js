9de38bbe5fa5e8b97a9c08b0e1a2340ed070c57d
Frknzdmr1 <ozdemirf97@icloud.com>
2022-10-17 22:11:25 -0400

    styling left

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+
+import { useState } from "react";
+import { Input, Button } from "reactstrap";
+import { data } from "./assets/data"
 import './App.css';
 
 function App() {
+  const [stateData, setStateData] = useState(data)
+  const [totalItems, setTotalItems] = useState(0)
+  const [totalPrice, setTotalPrice] = useState(0)
+  const [searchValue, setSearchValue] = useState('')
+
+  const calculateTotal = (arr) => {
+    return arr.reduce((acc, i) => {
+      return acc + i.count
+    }, 0)
+  }
+
+  const handleIncr = (index) => {
+    const newData = [...stateData]
+    newData[index].count++
+
+    // let total = newData.reduce((acc, i) => {
+    //   return acc + i.count
+    // }, 0)
+
+    let totalPrice = 0;
+    for (let i of newData) {
+      totalPrice += i.count * i.price
+    }
+    setTotalPrice(totalPrice)
+    setTotalItems(calculateTotal(newData))
+    setStateData(newData)
+  }
+  const handleDecr = (ind) => {
+
+    const newData = [...stateData]
+    if (newData[ind].count) {
+      newData[ind].count--
+
+      // let total = 0;
+      // for (let i of newData) {
+      //   total += i.count
+      // }
+      let totalPrice = 0;
+      for (let i of newData) {
+        totalPrice += i.count * i.price
+      }
+      setTotalPrice(totalPrice)
+      setTotalItems(calculateTotal(newData))
+      // setTotalItems(total)
+      setStateData(newData)
+    }
+  }
+  const handleSearch = (event) => {
+    setSearchValue(event.target.value)
+
+  }
+
+  const filtered = stateData.filter(i => i.title.toLowerCase().includes(searchValue.toLowerCase()))
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <Input value={searchValue} placeholder="search..." onChange={handleSearch} />
+        <span className="counter">{totalItems}</span>
+      </div>
+      <div className="main">
+        {filtered.map((item, index) => {
+          return (
+            <div className="custom-card">
+              <img src={item.image} alt="" width='70' />
+              <p>{item.title.slice(0, 12)}...</p>
+              <div>
+                <p>price: {item.price}</p>
+                <p>{item.count}</p>
+                <Button color="primary" onClick={() => handleIncr(index)}>+</Button>
+                <Button color='danger' onClick={() => handleDecr(index)}>-</Button>
+              </div>
+            </div>
+          )
+        })
+
+
+        }
+      </div>
+      <div style={{ marginBottom: "10px", fontWeight: "bold" }}>Total price : {totalPrice}$ </div>
+      <div>
+        <Button>prev</Button>
+        <Button>next</Button>
+      </div>
     </div>
   );
 }
